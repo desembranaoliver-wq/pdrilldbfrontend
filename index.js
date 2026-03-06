ba6cb2600a15efd9b78640b743119e845cb7b589
desembranaoliver-wq <desembranaoliver@gmail.com>
2026-03-06 12:28:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ submit.addEventListener("click", () => {
     quantity: document.querySelector("#quantity").value,
     supplier: document.querySelector("#supplier").value,
   };
-  fetch("https://gadgets-rc3z.onrender.com/api/products", {
+  fetch("https://pdrilldb.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(product),
@@ -42,6 +42,7 @@ submit.addEventListener("click", () => {
   alert("Product added successfully");
   location.reload();
 });
+
 
 
 
